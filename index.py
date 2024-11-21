24378d7192b15ee0a6dc3eab1edb142c6773e0a7
hashem <hashem@.com>
2024-11-21 17:38:25 +0100

    Initial commit

---
+++ index.py
@@ -0,0 +1,18 @@
+
+# print('hallo ')
+
+
+# print('hallo world again ')
+
+# print('hallo' ,'world','yet','again',sep="-",end='...\n')
+
+# Frist_name=input("enter Frist name : ",)
+# print(
+#     "Prints the values to a stream, or to sys.stdout by default. Optional \
+#     file: a file-like object (stream); defaults to the current sys.stdout.\
+#      sep: string inserted between values, default is a space.\
+#      end: string appended after the last value, default is a newline. \
+#     flush: whether to forcibly flush the stream."
+# )
+Frist_name=input("enter Frist name : ",)
+print("hello ",Frist_name,)
